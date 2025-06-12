ec5f26f2757bddd595f431a8faf17208831d2b15
joelobros23 <laberintoj8@gmail.com>
2025-06-13 05:06:12 +0800

    feat: Configure the database connection in api/config.php.

--- api/config.php
+++ api/config.php
@@ -1,3 +1,17 @@
 <?php
-// PHP code will be generated here.
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "chronoweave";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Set the Content-Type header to JSON for API responses
+header('Content-Type: application/json');
 ?>
